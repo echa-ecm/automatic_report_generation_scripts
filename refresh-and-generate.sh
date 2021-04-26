c27630c1500db4863fc870e95e1b10b7c75c6f77
Oscar S. Silva <oscar.silva@alumni.helsinki.fi>
2021-04-26 14:35:11 +0300

    trailing whitespace and accepted xml mime type

--- refresh-and-generate.sh
+++ refresh-and-generate.sh
@@ -5,15 +5,15 @@
 # Tested with IUCLID6 v5.13.0
 # env-file points to a copy of default.env with a set of settings
 # Tool to refresh a given ($REPORT_ID) template in a $SERVER and to generate reports for a  given list of UUIDs in DOSSIERS, SUBSTANCES, ARTICLES,
-# and MIXTURES 
+# and MIXTURES
 # IUCLID API expects a zip file that matches structure of https://dev.azure.com/echa-ecm/IUCLID/_git/iuclid6-ext?path=%2Fiuclid6-ext-adapter%2Fsrc%2Ftest%2Fresources%2Feu%2Fecha%2Fiuclid6%2Fext%2Fadapter%2Fcontext%2Freport.zip&_a=contents&version=GBmaster
-# The linked zip file provides a list of possible $WORKING_CONTEXTS 
+# The linked zip file provides a list of possible $WORKING_CONTEXTS
 # Make a copy of default.env and modify the values
 # Runs on git bash https://gitforwindows.org/
 # The program assumes all ftls and stylesheets are in the same $TEMPLATE_PATH
 # You need to create the folders ftl, mainFtl, styles, and output in $TEMP
 # The program expects zip to be in the PATH, ie, copy zip.exe from https://sourceforge.net/projects/gnuwin32/files/zip/3.0/
-# to [gitforbashpath(C:\Users\u18339\AppData\Local\Programs\Git\)]\git\mingw64\bin, 
+# to [gitforbashpath(C:\Users\u18339\AppData\Local\Programs\Git\)]\git\mingw64\bin,
 # also a copy of bzip2.dll is in need in the PATH for zip to work
 
 
@@ -46,7 +46,7 @@ done
 
 
 if [ "$RF_REPORT_OUTPUT" = "HTML" ];
-then    
+then
     RF_REPORT_EXTENSION=html
     RF_ACCEPT_CONTENT="text/html"
 elif [ "$RF_REPORT_OUTPUT" = "PDF" ];
@@ -64,8 +64,8 @@ then
 elif [ "$RF_REPORT_OUTPUT" = "XML" ];
 then
     RF_REPORT_EXTENSION=xml
-    RF_ACCEPT_CONTENT="text/xml"
-else 
+    RF_ACCEPT_CONTENT="application/xml"
+else
     echo "Specify RF_REPORT_OUTPUT to be one of HTML, PDF, CSV, RTF, XML"
 fi
 JOIN_BY () {
@@ -103,23 +103,23 @@ REFRESH_TEMPLATE () {
 
     # The zip created with bash created directory breaks IUCLID. Create these directories manually using git bash, and not windows file explorer
     # mkdir temp
-    # mkdir temp/ftl 
+    # mkdir temp/ftl
     # mkdir temp/mainFtl
     # mkdir temp/output
     # mkdir temp/styles
-    echo $RF_REPORT_NAME > "$RF_TEMP_PATH/name" 
+    echo $RF_REPORT_NAME > "$RF_TEMP_PATH/name"
     echo $RF_REPORT_DESC > "$RF_TEMP_PATH/description"
-    echo $RF_WORKING_CONTEXTS > "$RF_TEMP_PATH/workingContexts" 
+    echo $RF_WORKING_CONTEXTS > "$RF_TEMP_PATH/workingContexts"
     echo $RF_REPORT_OUTPUT > "$RF_TEMP_PATH/output/output"
     cp "$RF_TEMPLATE_PATH/$RF_MAIN_FTL.ftl" "$RF_TEMP_PATH/mainFtl"
 
-    for i in "${RF_FTLS[@]}"; 
-    do 
+    for i in "${RF_FTLS[@]}";
+    do
         cp "$RF_TEMPLATE_PATH/$i" "$RF_TEMP_PATH/ftl"
     done
 
     cp "$RF_STYLES_PATH/$RF_STYLES.xsl" "$RF_TEMP_PATH/styles"
-    
+
     rm "$RF_REPORT_ZIP_PATH"
     (cd "$RF_TEMP_PATH" && zip -r -q "package.zip" ./*)
 
@@ -141,26 +141,26 @@ then
     REFRESH_TEMPLATE
 fi
 
-for i in "${RF_DOSSIERS[@]}"; 
-do 
+for i in "${RF_DOSSIERS[@]}";
+do
     RF_GENERATE_URL=$RF_SERVER/iuclid6-ext/api/ext/v1/dossier/$i?template=database~$RF_REPORT_ID.$RF_MAIN_FTL
     CREATE_REPORT
 done
 
-for i in "${RF_MIXTURES[@]}"; 
-do 
+for i in "${RF_MIXTURES[@]}";
+do
     RF_GENERATE_URL=$RF_SERVER/iuclid6-ext/api/ext/v1/raw/MIXTURE/$i?template=database~$RF_REPORT_ID.$RF_MAIN_FTL
     CREATE_REPORT
 done
 
-for i in "${RF_SUBSTANCES[@]}"; 
-do 
+for i in "${RF_SUBSTANCES[@]}";
+do
     RF_GENERATE_URL=$RF_SERVER/iuclid6-ext/api/ext/v1/raw/SUBSTANCE/$i?template=database~$RF_REPORT_ID.$RF_MAIN_FTL
     CREATE_REPORT
 done
 
-for i in "${RF_ARTICLES[@]}"; 
-do 
+for i in "${RF_ARTICLES[@]}";
+do
     RF_GENERATE_URL=$RF_SERVER/iuclid6-ext/api/ext/v1/raw/ARTICLE/$i?template=database~$RF_REPORT_ID.$RF_MAIN_FTL
     CREATE_REPORT
 done
